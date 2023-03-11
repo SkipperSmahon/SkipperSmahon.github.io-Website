9c567cc3f9a96fd85f1fb23f77f1c62a6ea5a50c
SkipperSmahon <skipper_smahon@hotmail.com>
2023-03-11 23:13:52 +0800

    First commit

---
+++ precache-manifest.b2375f34135727bfb1e63ee1e2d4d920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "013c92d5ac958c873f4b",
+    "url": "/CloudResume/static/css/main.fd179b21.chunk.css"
+  },
+  {
+    "revision": "013c92d5ac958c873f4b",
+    "url": "/CloudResume/static/js/main.013c92d5.chunk.js"
+  },
+  {
+    "revision": "6d5011ca78e5fe9fcc3a",
+    "url": "/CloudResume/static/js/1.6d5011ca.chunk.js"
+  },
+  {
+    "revision": "b65ca77347ae0a2d1b4d",
+    "url": "/CloudResume/static/js/2.b65ca773.chunk.js"
+  },
+  {
+    "revision": "0dac40e3c52ce62b751c",
+    "url": "/CloudResume/static/js/runtime~main.0dac40e3.js"
+  },
+  {
+    "revision": "5930172350c6c6699c1067f9681558c2",
+    "url": "/CloudResume/index.html"
+  }
+];
